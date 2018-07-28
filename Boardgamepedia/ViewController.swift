ae7215be3752e6f186c06d251afef7e8fe579ed7
謝清荃 <shane72037@hotmail.com>
2018-07-29 03:07:08 +0800

    Initial Commit

---
+++ Boardgamepedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boardgamepedia
+//
+//  Created by 謝清荃 on 2018/7/29.
+//  Copyright © 2018年 Shane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
